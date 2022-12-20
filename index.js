ad437536fe1ce9234e77cf72a6bd2ce84baa2691
Accsandoval56 <accsandoval56@gmail.com>
2022-12-19 21:49:51 -0800

    lets go

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
-// const canvas = document.querySelector('cvs');
-// const ctx = canvas.getContext('2d');
-
-// canvas.width = 288
-// canvas.height = 512 
-
-// ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-// const bird = new Image() ; 
-// const bg = new Image(); 
-// const fg = new Image(); 
-// const pipeNorth = new Image();  
-// const pipeSouth = new Image(); 
-
-// bird.src = "images/bird.png";
-// bg.src = "images/bg.png";
-// fg.src = "images/fg.png";
-// pipeNorth.src = "images/pipeNorth.png";
-// pipeSouth.src = "images/pipeSouth.png"; 
-
-
-ctx = cvs.getContext("2d"); // canvas gets placed a variable ctx // 
-const bird = new Image(); // DOM selecting the node bird.png // 
-bird.src = "images/bird.png"
-birdX = birDY = 0;
+ctx = cvs.getContext("2d"); // canvas gets placed a variable named  ctx //
+const bird = new Image(); // DOM bird.png // 
+bird.src = "images/bird.png" 
+birdX = birDY = score = bestScore = 0;
 interval = birdSize = pipeWidth = topPipeBottomY = 24;
 birdY = pipeGap = 200;
 canvasSize = pipeX = 400;
-cvs.onclick = () => (birDY = 9) // the onclick method to fly 9 // 
-
+cvs.onclick = () => (birDY = 9) // onclick method fly set to 9 
 setInterval(() => {
-    ctx.fillStyle = "skyblue"
-    ctx.fillRect(0, 0, canvasSize, canvasSize) // draw sky // 
-    birdY -= birDY -= 0.5;   // gravity is set to 0.5 // 
-    ctx.drawImage(bird, birdX, birdY, birdSize * (524 / 374), birdSize); //draw bird // 
-    ctx.fillStyle = "green";
-    pipeX -= 5; // left to right speed set to 5 // 
-    pipeX < -pipeWidth && ((pipeX = canvasSize), (topPipeBottomY = pipeGap * Math.random())); 
-    ctx.fillRect(pipeX, 0, pipeWidth, topPipeBottomY); // draw top pipe // 
-    ctx.fillRect(pipeX, topPipeBottomY + pipeGap, pipeWidth, canvasSize); // draw bottom pipe // 
+    ctx.fillStyle = "skyblue";
+    ctx.fillRect(0, 0, canvasSize, canvasSize); // draw sky 
+    birdY -= birDY -= 0.5;   // gravity is set to 0.5 
+    ctx.drawImage(bird, birdX, birdY, birdSize * (524 / 374), birdSize); //draw bird 
+    ctx.fillStyle = "seagreen";
+    pipeX -= 8; // speed (left to right) set to 5 // 
+    pipeX < -pipeWidth &&
+    ((pipeX = canvasSize), (topPipeBottomY = pipeGap * Math.random()));
+    ctx.fillRect(pipeX, 0, pipeWidth, topPipeBottomY); // draw top pipe  
+    ctx.fillRect(pipeX, topPipeBottomY + pipeGap, pipeWidth, canvasSize); // draw bottom pipe 
+    ctx.fillStyle = "black";
+    ctx.fillText(score++, 9, 25); 
+    bestScore = bestScore < score ? score : bestScore; 
+    ctx.fillText(`Best Score : ${bestScore}`, 9, 50);
+    console.log(bestScore); 
 
-    (birdY > canvasSize) && ((birDY = 0), (birdY = 200)); // bird dies past the canvas screen //
+    (((birdY < topPipeBottomY || birdY > topPipeBottomY + pipeGap) && pipeX < birdSize * (524 / 374)) // bird hit pipe 
+    || birdY > canvasSize) &&  // bird falls off screen 
+    ((birDY = 0), (birdY = 200), (pipeX = canvasSize), (score =0)); // bird died  
 }, interval)
---
+++ leaderboard.js
@@ -0,0 +1,3 @@
+ctx2 = cvs2.getContext("2d"); // canvas gets placed a variable named  ctx //
+
+    ctx2.fillRect(0, 0, canvasSize, canvasSize); // draw sky 
